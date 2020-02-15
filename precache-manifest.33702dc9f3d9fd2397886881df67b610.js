f5ff3c311d9c4ee03ca314947ccf5eabf6fd087a
atahanszgn <atahan265@gmail.com>
2020-02-15 16:32:08 +0300

    Updates

---
+++ precache-manifest.33702dc9f3d9fd2397886881df67b610.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30f6e4c9fa1b75e6b65b40f5b76ec67",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "855f7bf030b19d464399",
+    "url": "/portfolio/static/css/main.4a2fcbe0.chunk.css"
+  },
+  {
+    "revision": "e5dc7abed26783fb3e76",
+    "url": "/portfolio/static/js/2.0def9675.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.0def9675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855f7bf030b19d464399",
+    "url": "/portfolio/static/js/main.ed902e8b.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "cac91df566aa6c9d2010e948d3a61339",
+    "url": "/portfolio/static/media/me.cac91df5.png"
+  },
+  {
+    "revision": "9d0333281e1e06e03ce618c7e24bd3e7",
+    "url": "/portfolio/static/media/roboSS.9d033328.png"
+  }
+]);
